bb3e4853fc5255e3d084fd52d998139d5d286479
M Junaidi <mjunaidi@Ms-MacBook-Pro.local>
2019-02-03 04:54:57 +0800

    Preparing for github pages.

---
+++ docs/precache-manifest.b0dead524b783780d9623e16b732b934.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d21127fa804121f390b3",
+    "url": "/static/js/main.d21127fa.chunk.js"
+  },
+  {
+    "revision": "5af9d063d811ce383825",
+    "url": "/static/js/1.5af9d063.chunk.js"
+  },
+  {
+    "revision": "d21127fa804121f390b3",
+    "url": "/static/css/main.f3b9cf4f.chunk.css"
+  },
+  {
+    "revision": "5af9d063d811ce383825",
+    "url": "/static/css/1.e9c4f6ed.chunk.css"
+  },
+  {
+    "revision": "b225cf711ce7c24e4ad4b2a78d121864",
+    "url": "/index.html"
+  }
+];
