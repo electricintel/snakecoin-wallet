7507191f792e4561c0b60b81ea6390805d6c0251
M Junaidi <mjunaidi@Ms-MacBook-Pro.local>
2019-02-03 05:08:55 +0800

    Updates

---
+++ precache-manifest.8cbbdccf9b3fd95249046650ec666b89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b5fe61ad2af937f3cb25",
+    "url": "/snakecoin-wallet/static/js/runtime~main.b5fe61ad.js"
+  },
+  {
+    "revision": "decec54bf192e7ef9b99",
+    "url": "/snakecoin-wallet/static/js/main.decec54b.chunk.js"
+  },
+  {
+    "revision": "30f08afedaee7a58eb82",
+    "url": "/snakecoin-wallet/static/js/1.30f08afe.chunk.js"
+  },
+  {
+    "revision": "decec54bf192e7ef9b99",
+    "url": "/snakecoin-wallet/static/css/main.f3b9cf4f.chunk.css"
+  },
+  {
+    "revision": "30f08afedaee7a58eb82",
+    "url": "/snakecoin-wallet/static/css/1.e9c4f6ed.chunk.css"
+  },
+  {
+    "revision": "444675da862268a02f88f55e105f10f1",
+    "url": "/snakecoin-wallet/index.html"
+  }
+];
